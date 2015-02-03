9a787aec9ae38512dc77fc576f9f6045a4a2b957
ozgecnkr <ozgecnkr@hotmail.com>
2015-02-03 11:05:01 +0200

    taslak eklendi

---
+++ taslak.rb
@@ -0,0 +1,43 @@
+puts "**************************************************************"
+puts "*                  --Patpat--                                *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "*                  WELCOME!!                                 *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "*                 Oyunun Amacı:                              *"
+puts "*   x ve y koordinatlarını girerek haritadaki şekli bulmak.  *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "*   -Başlamak için enter'a basınız-                          *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "*                                                            *"
+puts "**************************************************************"
+
+unless gets.empty?
+puts "x ve y koordinatlarını giriniz: "
+
+end
+
+if
+puts "Tebrikler üçgeni patpatladınız: "
+
+end
+
+if
+puts "Tebrikler kareyi patpatladınız: "
+
+end
+
+if
+puts "Tebrikler dikdörtgeni patpatladınız: "
+
+end
+
+
+if
+puts "Upps.Tekrar deneyiniz." 
+
+end
