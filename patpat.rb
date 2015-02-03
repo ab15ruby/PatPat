2101a11a1655991b394d7fb4abeadac61c61bc6b
peymannn <peyman_oter_468@hotmail.com>
2015-02-03 12:19:19 +0200

    puanlama sistemi olusturuldu

--- patpat.rb
+++ patpat.rb
@@ -9,10 +9,27 @@ class Patpat
   def initialize(size)
     @table = Table.new(size)
   end
+  def puanlama (x,y) 
+        if table.bosmu?(x,y)
+        	return "0"
+        
+        elsif table.bosmu? (x+1, y)  and (x-1,y) and (x,y-1) and (x,y-1)  #ucgen için
+                return '1'
 
+         elsif table.bosmu?(x-2, y) and (x+2,y) and (x,y-2) and (x,y+2) # kare için
 
-
-
+         	return '2'
+         elsif table.bosmu? (x-3,y) and (x+3,y) and (x,y-3) and (x,y+3) and (x-1,y+1) and (x-1,y-1) and (x+1,y+1) and (x+1,y-1) #dikdortgen için 
+         	return '4'
+         elsif 
+         	
+         		
+         
+              break
+            else 
+              redo 
+            end
+        end
 end
 
 
